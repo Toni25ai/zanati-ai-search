0cd04af46895fca2c845a47431482543f6fe8443
Toni25ai <tonigega22@gmail.com>
2025-11-30 22:08:38 +0100

    Update api.py

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 # ========== SUPABASE CONNECT ==========
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPBASE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # ========== OPENAI CONNECT ==========
